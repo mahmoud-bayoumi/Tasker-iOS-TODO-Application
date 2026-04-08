b9b1a00aa4767380fb7fd73d6035f9acba209bcc
Mahmoud Mohamed <mahmoudbayoumi110@gmail.com>
2026-04-08 02:28:59 -0700

    add attach file feature

--- TODO/TODO/AddTaskViewController.m
+++ TODO/TODO/AddTaskViewController.m
@@ -8,6 +8,7 @@
 #import "Task.h"
 #import "TaskManager.h"
 #import <UserNotifications/UserNotifications.h>
+#import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
 
 @interface AddTaskViewController ()
 
@@ -16,17 +17,19 @@
 @property (weak, nonatomic) IBOutlet UISegmentedControl *prioritySegment;
 
 @property (weak, nonatomic) IBOutlet UIDatePicker *reminderDatePicker;
-
 @property (weak, nonatomic) IBOutlet UILabel *reminderAtLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *fileNameLabel;
 
 @property (nonatomic, assign) TaskPriority selectedPriority;
 @property (nonatomic, assign) BOOL reminderSet;
 @property (nonatomic, strong) NSDate *originalPickerDate;
+@property (nonatomic, strong) NSString *attachedFilePath;
+@property (nonatomic, strong) NSString *attachedFileName;
 
 @end
 
 @implementation AddTaskViewController
-
 
 
 - (void)viewDidLoad {
@@ -42,12 +45,11 @@
                 style:UIBarButtonItemStylePlain
                target:self
                action:@selector(cancelTapped)];
-
+    
     self.reminderDatePicker.date = [NSDate date];
     self.reminderDatePicker.minimumDate = [NSDate date];
     self.reminderDatePicker.datePickerMode = UIDatePickerModeDateAndTime;
-    self.reminderDatePicker.hidden = NO;  // Always visible
-    
+    self.reminderDatePicker.hidden = NO;
     self.originalPickerDate = self.reminderDatePicker.date;
     
     [self.reminderDatePicker addTarget:self
@@ -57,11 +59,15 @@
     self.reminderSet = NO;
     self.reminderAtLabel.hidden = YES;
     
-    // ── Request Notification Permission ──
+    self.attachedFilePath = nil;
+    self.attachedFileName = nil;
+    self.fileNameLabel.hidden = YES;
+    
     [self requestNotificationPermission];
     
     NSLog(@"AddTaskViewController loaded");
 }
+
 
 
 - (IBAction)prioritySegmentAction:(UISegmentedControl *)sender {
@@ -74,66 +80,48 @@
 }
 
 
-
 - (void)datePickerChanged:(UIDatePicker *)picker {
-    
     self.reminderSet = YES;
-    
     [self updateReminderLabel];
     self.reminderAtLabel.hidden = NO;
-    
-    NSLog(@"User changed date → Reminder will be set");
 }
 
 - (void)updateReminderLabel {
     NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
     fmt.dateFormat = @"MMM dd, yyyy 'at' hh:mm a";
     fmt.timeZone = [NSTimeZone localTimeZone];
-    
     self.reminderAtLabel.text = [NSString stringWithFormat:
         @"Reminder: %@", [fmt stringFromDate:self.reminderDatePicker.date]];
 }
 
-
 - (IBAction)removeReminderAction:(id)sender {
-
     self.reminderSet = NO;
     self.reminderAtLabel.hidden = YES;
-    
     self.reminderDatePicker.date = [NSDate date];
     self.originalPickerDate = self.reminderDatePicker.date;
-    
-    NSLog(@"Reminder removed, picker reset to now");
+    NSLog(@"Reminder removed");
 }
 
-// ── Keep setReminderAction if button exists (optional) ──
 - (IBAction)setReminderAction:(id)sender {
-    // If user taps "Set Reminder" button, treat as manual set
     self.reminderSet = YES;
     [self updateReminderLabel];
     self.reminderAtLabel.hidden = NO;
-    
-    NSLog(@"Reminder manually set");
 }
 
 
 - (void)requestNotificationPermission {
     UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-    
     [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert |
                                              UNAuthorizationOptionSound |
                                              UNAuthorizationOptionBadge)
                           completionHandler:^(BOOL granted, NSError *error) {
         if (granted) {
             NSLog(@"Notification permission granted");
-        } else {
-            NSLog(@"Notification permission denied");
         }
     }];
 }
 
 - (void)scheduleReminderNotification:(Task *)task {
-    
     if (!task.reminderDate) return;
     
     UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
@@ -150,16 +138,11 @@
         triggerWithDateMatchingComponents:components repeats:NO];
     
     UNNotificationRequest *request = [UNNotificationRequest
-        requestWithIdentifier:task.taskID
-                      content:content
-                      trigger:trigger];
+        requestWithIdentifier:task.taskID content:content trigger:trigger];
     
     [[UNUserNotificationCenter currentNotificationCenter]
-        addNotificationRequest:request
-         withCompletionHandler:^(NSError *error) {
-        if (error) {
-            NSLog(@"Notification error: %@", error.localizedDescription);
-        } else {
+        addNotificationRequest:request withCompletionHandler:^(NSError *error) {
+        if (!error) {
             NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
             fmt.dateFormat = @"MMM dd, yyyy 'at' hh:mm a";
             fmt.timeZone = [NSTimeZone localTimeZone];
@@ -167,6 +150,159 @@
                   [fmt stringFromDate:task.reminderDate]);
         }
     }];
+}
+
+
+- (IBAction)attachFileAction:(id)sender {
+    
+    UIAlertController *sheet = [UIAlertController
+        alertControllerWithTitle:@"Attach File"
+        message:@"Choose a source"
+        preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    [sheet addAction:[UIAlertAction
+        actionWithTitle:@"Choose Document"
+        style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction *action) {
+            [self openDocumentPicker];
+    }]];
+    
+    [sheet addAction:[UIAlertAction
+        actionWithTitle:@"Choose Photo"
+        style:UIAlertActionStyleDefault
+        handler:^(UIAlertAction *action) {
+            [self openPhotoPicker];
+    }]];
+    
+    [sheet addAction:[UIAlertAction
+        actionWithTitle:@"Cancel"
+        style:UIAlertActionStyleCancel
+        handler:nil]];
+    
+    [self presentViewController:sheet animated:YES completion:nil];
+}
+
+
+
+- (void)openDocumentPicker {
+    NSArray *types = @[UTTypeItem, UTTypePDF, UTTypeImage, UTTypePlainText];
+    
+    UIDocumentPickerViewController *picker = [[UIDocumentPickerViewController alloc]
+        initForOpeningContentTypes:types];
+    picker.delegate = self;
+    picker.allowsMultipleSelection = NO;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)documentPicker:(UIDocumentPickerViewController *)controller
+    didPickDocumentsAtURLs:(NSArray<NSURL *> *)urls {
+    
+    NSURL *url = urls.firstObject;
+    if (!url) return;
+    
+    [url startAccessingSecurityScopedResource];
+    
+    NSString *docsPath = NSSearchPathForDirectoriesInDomains(
+        NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+    NSString *fileName = url.lastPathComponent;
+    NSString *destPath = [docsPath stringByAppendingPathComponent:fileName];
+    
+    [[NSFileManager defaultManager] removeItemAtPath:destPath error:nil];
+    
+    NSError *error = nil;
+    [[NSFileManager defaultManager] copyItemAtURL:url
+        toURL:[NSURL fileURLWithPath:destPath] error:&error];
+    
+    [url stopAccessingSecurityScopedResource];
+    
+    if (!error) {
+        [self fileAttachedWithPath:destPath fileName:fileName];
+    } else {
+        NSLog(@"File copy error: %@", error.localizedDescription);
+    }
+}
+
+- (void)documentPickerWasCancelled:(UIDocumentPickerViewController *)controller {
+    NSLog(@"Document picker cancelled");
+}
+
+
+- (void)openPhotoPicker {
+    if (![UIImagePickerController isSourceTypeAvailable:
+          UIImagePickerControllerSourceTypePhotoLibrary]) {
+        
+        UIAlertController *alert = [UIAlertController
+            alertControllerWithTitle:@"Not Available"
+            message:@"Photo library is not available."
+            preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"OK"
+            style:UIAlertActionStyleDefault handler:nil]];
+        [self presentViewController:alert animated:YES completion:nil];
+        return;
+    }
+    
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.delegate = self;
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info {
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+    if (!image) return;
+    
+    NSString *docsPath = NSSearchPathForDirectoriesInDomains(
+        NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+    NSString *fileName = [NSString stringWithFormat:@"photo_%@.jpg",
+        [[NSUUID UUID] UUIDString]];
+    NSString *destPath = [docsPath stringByAppendingPathComponent:fileName];
+    
+    NSData *imageData = UIImageJPEGRepresentation(image, 0.8);
+    BOOL success = [imageData writeToFile:destPath atomically:YES];
+    
+    if (success) {
+        [self fileAttachedWithPath:destPath fileName:fileName];
+    } else {
+        NSLog(@"Failed to save photo");
+    }
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+- (void)fileAttachedWithPath:(NSString *)path fileName:(NSString *)name {
+    
+    if (self.attachedFilePath) {
+        [[NSFileManager defaultManager] removeItemAtPath:self.attachedFilePath error:nil];
+    }
+    
+    self.attachedFilePath = path;
+    self.attachedFileName = name;
+    
+    self.fileNameLabel.text = [NSString stringWithFormat:@"%@", name];
+    self.fileNameLabel.hidden = NO;
+    
+    NSLog(@"File attached: %@", name);
+}
+
+- (IBAction)removeFileAction:(id)sender {
+    
+    if (self.attachedFilePath) {
+        [[NSFileManager defaultManager] removeItemAtPath:self.attachedFilePath error:nil];
+    }
+    
+    self.attachedFilePath = nil;
+    self.attachedFileName = nil;
+    
+    self.fileNameLabel.hidden = YES;
+    
+    NSLog(@"File removed");
 }
 
 
@@ -204,10 +340,13 @@
     
     if (self.reminderSet) {
         task.reminderDate = self.reminderDatePicker.date;
-        NSLog(@"Reminder will be set for this task");
     } else {
         task.reminderDate = nil;
-        NSLog(@"No reminder — date picker was not changed");
+    }
+    
+    if (self.attachedFilePath) {
+        task.attachedFilePath = self.attachedFilePath;
+        task.attachedFileName = self.attachedFileName;
     }
     
     [[TaskManager sharedManager] addTask:task];
@@ -216,9 +355,10 @@
         [self scheduleReminderNotification:task];
     }
     
-    NSLog(@"Task Created: %@ | Priority: %ld | Reminder: %@",
+    NSLog(@"Task Created: %@ | Priority: %ld | Reminder: %@ | File: %@",
           task.name, (long)task.priority,
-          self.reminderSet ? @"YES" : @"NO");
+          self.reminderSet ? @"YES" : @"NO",
+          self.attachedFileName ?: @"NONE");
     
     NSMutableString *message = [NSMutableString stringWithFormat:
         @"\"%@\" has been added to your list.", name];
@@ -227,8 +367,12 @@
         NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
         fmt.dateFormat = @"MMM dd, yyyy 'at' hh:mm a";
         fmt.timeZone = [NSTimeZone localTimeZone];
-        [message appendFormat:@"\n\nReminder set for:\n%@",
+        [message appendFormat:@"\n\nReminder: %@",
             [fmt stringFromDate:self.reminderDatePicker.date]];
+    }
+    
+    if (self.attachedFileName) {
+        [message appendFormat:@"\n\n📎 File: %@", self.attachedFileName];
     }
     
     UIAlertController *success = [UIAlertController
@@ -251,8 +395,9 @@
     BOOL hasTitle    = (self.titleTextField.text.length > 0);
     BOOL hasDesc     = (self.descriptionTextField.text.length > 0);
     BOOL hasReminder = self.reminderSet;
+    BOOL hasFile     = (self.attachedFilePath != nil);
     
-    if (hasTitle || hasDesc || hasReminder) {
+    if (hasTitle || hasDesc || hasReminder || hasFile) {
         UIAlertController *alert = [UIAlertController
             alertControllerWithTitle:@"Discard Task?"
             message:@"You have unsaved changes. Are you sure?"
@@ -264,6 +409,11 @@
         [alert addAction:[UIAlertAction actionWithTitle:@"Discard"
             style:UIAlertActionStyleDestructive
             handler:^(UIAlertAction *action) {
+                // Clean up file
+                if (self.attachedFilePath) {
+                    [[NSFileManager defaultManager]
+                        removeItemAtPath:self.attachedFilePath error:nil];
+                }
                 [self.navigationController popViewControllerAnimated:YES];
         }]];
         
@@ -272,6 +422,8 @@
         [self.navigationController popViewControllerAnimated:YES];
     }
 }
+
+
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self.view endEditing:YES];
